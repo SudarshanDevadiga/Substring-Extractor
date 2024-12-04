3d7dda04321326026e39b8a21ec270f20694ee8f
SudarshanDevadiga <sudarshandevadiga200@gmail.com>
2024-12-04 11:59:59 +0530

    Add files via upload

---
+++ subsetFunc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1="Iomhar";
+    string r = s1.substr(3,2);
+    cout << "string is: "<< r;
+    return 0;
+}
